3c23b871cf6ce84270bd9f14a6877060b1023d16
Ben Pellow <benjamin.pellow@gmail.com>
2013-11-11 20:29:52 -0800

    log initializer

---
+++ config/initializers/lob.rb
@@ -0,0 +1,6 @@
+Lob.configure do |config|
+  config.api_key = "test_0516e9a1060ad9b9a60c63104ed94b21e23"  # test
+  config.api_version = "v1"        # default version
+  config.protocol = "https"        # default protocol
+  config.api_host = "api.lob.com"  # ofcourse it's Lob
+end
