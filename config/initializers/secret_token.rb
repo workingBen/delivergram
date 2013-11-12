b0db279cb7d34939502fa82153497f796946b15c
Ben Pellow <benjamin.pellow@gmail.com>
2013-11-11 18:49:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Delivergram::Application.config.secret_key_base = '509423b2135d49fd0d574c5468de39d540b4398ad6b063f62090a2819d486aecb735927e5cfd5824320049c6644b02bbc66f3d4748694261bd4ef7de1c20de3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Delivergram::Application.config.session_store :cookie_store, key: '_delivergram_session'
