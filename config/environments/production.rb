8bcd0d71fc88fa995d44d39123ce670c773e8e63
Ben Pellow <benjamin.pellow@gmail.com>
2013-11-11 20:11:25 -0800

    title font, Dumb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Delivergram::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Add the fonts path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 end
